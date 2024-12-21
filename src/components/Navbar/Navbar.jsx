9635fed96b2fe8444eb7955c3fa18e3d60430626
William Sheere IV <Sheerebilly@yahoo.com>
2024-12-21 16:36:59 -0500

    aboutme,nav,css,img,highlight,bio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./NavBar.css";
 
 function Navbar() {
-  return (
+	const location = useLocation();
+	console.log(location);
+	return (
 		<header>
 			<nav className="navbar">
 				{/*    <ul>
@@ -19,14 +21,42 @@ function Navbar() {
         </ul>
         */}
 				<div className="nav-links">
-					<Link to="/">Home</Link>
-					<Link to="/contact">Contact</Link>
-					<Link to="/portfolio">Portfolio</Link>
-					<Link to="/resume">Resume</Link>
+					<Link
+						className={
+							location.pathname === "/" ? "activeLink" : "inactiveLink"
+						}
+						to="/"
+					>
+						Home
+					</Link>
+					<Link
+						className={
+							location.pathname === "/contact" ? "activeLink" : "inactiveLink"
+						}
+						to="/contact"
+					>
+						Contact
+					</Link>
+					<Link
+						className={
+							location.pathname === "/portfolio" ? "activeLink" : "inactiveLink"
+						}
+						to="/portfolio"
+					>
+						Portfolio
+					</Link>
+					<Link
+						className={
+							location.pathname === "/resume" ? "activeLink" : "inactiveLink"
+						}
+						to="/resume"
+					>
+						Resume
+					</Link>
 				</div>
 			</nav>
 		</header>
 	);
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import img from "../assets/MK.jpg"
+import "./Aboutme.css"
 function Aboutme() {
   return (
-    <div>ABOUT ME</div>
-  )
+		<div className="aboutMeContainer">
+			<section>
+				<img src={img} className="bioImg" alt="about me image" />
+			</section>
+			<section>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+					ipsa nostrum magnam sint, ea doloribus, blanditiis molestias modi
+					dolorum consequuntur illum? Autem laudantium eum similique temporibus,
+					porro magnam omnis msu!Lorem ipsum dolor amet mustache knausgaard +1,
+					blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub
+					cornhole celiac swag. Brunch raclette vexillologist post-ironic
+					glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag.
+					Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue
+					bottle ennui tbh succulents.Zombie ipsum reversus ab viral inferno,
+					nam rick grimes malum cerebro. De carne lumbering animata corpora
+					quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi
+					gorger omero undead survivor dictum mauris. Hi mindless mortuis
+					soulless creaturas, imo evil stalking monstra adventus resi dentevil
+					vultus comedat cerebella viventium.Bacon ipsum dolor amet short ribs
+					brisket venison rump drumstick pig sausage prosciutto chicken spare
+					ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola
+					venison turkey shoulder picanha ham pork tri-tip meatball meatloaf
+					ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky
+					brisket pastrami shank.
+				</p>
+			</section>
+		</div>
+	);
 }
 
 export default Aboutme
