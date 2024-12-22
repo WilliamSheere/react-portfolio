a4b43c65954d0dc5737abbf681dc655a7867d008
William Sheere IV <Sheerebilly@yahoo.com>
2024-12-22 18:43:56 -0500

    removing node modules

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import placeholder from "../assets/placeholder.png";
+import "./Portfolio.css"
 function Portfolio() {
-  return (
-    <div>Portfolio</div>
-  )
+	const projects = [
+		{
+			img: placeholder,
+			title: "Git-Workshop",
+			repoLink: "https://github.com/WilliamSheere/git-workshop",
+			deployLink: "",
+		},
+		{
+			img: placeholder,
+			title: "Employee-Tracker",
+			repoLink: "https://github.com/WilliamSheere/git-workshop",
+			deployLink: "",
+		},
+		{
+			img: placeholder,
+			title: "Git-Workshop",
+			repoLink: "https://github.com/WilliamSheere/git-workshop",
+			deployLink: "",
+		},
+		{
+			img: placeholder,
+			title: "Git-Workshop",
+			repoLink: "https://github.com/WilliamSheere/git-workshop",
+			deployLink: "",
+		},
+		{
+			img: placeholder,
+			title: "Git-Workshop",
+			repoLink: "https://github.com/WilliamSheere/git-workshop",
+			deployLink: "",
+		},
+		{
+			img: placeholder,
+			title: "Git-Workshop",
+			repoLink: "https://github.com/WilliamSheere/git-workshop",
+			deployLink: "",
+		},
+	];
+	return (
+		<div>
+			<div>Portfolio</div>
+			<div>
+				{projects.map((project) => {
+					return (
+						<div className="projectCard">
+							<h2>{project.title}</h2>
+              <a href={project.repoLink}>
+              <img src={project.img} alt="" />
+
+              </a>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
-export default Portfolio
+export default Portfolio;
